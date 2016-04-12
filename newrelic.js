6ef3d26f8e37a7af4523034b033dd3331ccb9f28
Anatoly Chichikov <treks.mbox@gmail.com>
2016-04-12 17:40:24 +0300

    new relic config

--- newrelic.js
+++ newrelic.js
@@ -8,11 +8,11 @@ exports.config = {
   /**
    * Array of application names.
    */
-  app_name: ['pumpkin-tart-71750'],
+  app_name: ['dry-refuge-40691'],
   /**
    * Your New Relic license key.
    */
-  license_key: 'eee61b7f7f91dd27420975c00ae02d8f97bb6922',
+  license_key: 'd207b00e39c99689c2b6d7c8bb203245f4354d6e',
   logging: {
     /**
      * Level at which to log. 'trace' is most useful to New Relic when diagnosing
